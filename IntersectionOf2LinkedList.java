8b19fb4e8032788948615f219d74a97f20b4e3e0
Kanika Sahu <142916615+Kanika725@users.noreply.github.com>
2024-08-20 23:00:04 +0530

    Add files via upload

---
+++ IntersectionOf2LinkedList.java
@@ -0,0 +1,27 @@
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class IntersectionOf2LinkedList {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+    ListNode d1 = headA;
+    ListNode d2 = headB;
+    
+    while(d1 != d2) {
+        d1 = d1 == null? headB:d1.next;
+        d2 = d2 == null? headA:d2.next;
+    }
+    
+    return d1;
+}
+}
+
